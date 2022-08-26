a9d93aa14fe7a6d20b2db0bc04269aab3cded012
darryl west <dpw@raincitysoftware.com>
2022-08-26 11:33:51 -0700

    factorial

---
+++ scripts/factorial.rs
@@ -0,0 +1,14 @@
+#!/usr/bin/env rust-script
+// cargo-deps: rand
+
+
+fn main() {
+  let mut result: f64 = 1.0;
+
+  for n in (2..21).rev() {
+    result = result * (n as f64);
+    println!("{}:{}", n, result);
+  }
+
+  println!("result: {}", result);
+}
