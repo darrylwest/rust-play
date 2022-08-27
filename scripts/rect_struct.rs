c65a2d1d1fd3d1652ab365547678475e3f983843
darryl west <dpw@raincitysoftware.com>
2022-08-27 06:31:46 -0700

    added simple struct

---
+++ scripts/rect_struct.rs
@@ -0,0 +1,32 @@
+#!/usr/bin/env rust-script
+// cargo-deps: rand
+
+#[derive(Debug)]
+
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn new(width: u32, height: u32) -> Self {
+        Self {
+            width: width,
+            height: height,
+        }
+    }
+}
+
+fn main() {
+    let rect = Rectangle::new(30, 50);
+
+    println!(
+        "Rectangle area is {:?} square pixels.", 
+        rect.area()
+    );
+}
+
