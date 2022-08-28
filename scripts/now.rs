69e27e6481c0f8bffcb325aa79356610e8e37f00
darryl west <dpw@raincitysoftware.com>
2022-08-28 10:52:42 -0700

    rustfmt

--- scripts/now.rs
+++ scripts/now.rs
@@ -6,5 +6,10 @@ use chrono::prelude::*;
 fn main() {
     let utc: DateTime<Utc> = Utc::now();
 
-    println!("{} {:x} {}", utc.timestamp(), utc.timestamp(), utc.to_rfc3339());
+    println!(
+        "{} {:x} {}",
+        utc.timestamp(),
+        utc.timestamp(),
+        utc.to_rfc3339()
+    );
 }
