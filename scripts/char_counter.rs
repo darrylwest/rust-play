3e8c1578cfbff44a85fdbb7e2eb1fab93772f5c3
darryl west <dpw@raincitysoftware.com>
2022-08-28 07:46:48 -0700

    added word count

--- scripts/char_counter.rs
+++ scripts/char_counter.rs
@@ -5,7 +5,9 @@ use std::collections::HashMap;
 fn main() {
   let mut letters = HashMap::new();
 
-  for ch in "this is a test magazine article with lots of messages".chars() {
+  let s = "this is a test magazine article with lots and lots and lots of messages";
+
+  for ch in s.chars() {
     if ch != ' ' {
       letters.entry(ch).and_modify(|count| *count += 1).or_insert(1);
     }
@@ -14,4 +16,12 @@ fn main() {
   for (key, val) in letters.iter() {
     println!("{} count: {}", key, val);
   }
+
+  let mut words = HashMap::new();
+  for word in s.split_whitespace() {
+      let count = words.entry(word).or_insert(0);
+      *count += 1;
+  }
+
+  println!("Word count: {:?}", words);
 }
