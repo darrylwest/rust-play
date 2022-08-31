ac288db704990c2653d96ae402e1d436d9e1ce8a
darryl west <dpw@raincitysoftware.com>
2022-08-31 14:21:21 -0700

    added function pointer impl

---
+++ scripts/func_pointer.rs
@@ -0,0 +1,28 @@
+#!/usr/bin/env rust-script
+
+fn create_adder() -> Box<dyn Fn(i32, i32) -> i32> {
+    Box::new(|x, y| x + y)
+}
+
+fn create_stepper(increment: i32) -> Box<dyn Fn(&i32) -> i32> {
+    Box::new(move |x| *x + increment)
+}
+
+fn main() {
+    let x = 5;
+    let y = 3;
+    
+    let adder = create_adder();
+    
+    println!("{} + {} = {}", x, y, adder(x, y));
+
+    let stepper = create_stepper(2);
+    let mut z = 20;
+
+    for _ in 0..5 {
+        z = stepper(&z);
+
+        println!("next step -> {}", z);
+    }
+    
+}
