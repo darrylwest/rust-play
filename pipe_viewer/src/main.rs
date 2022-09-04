9daad76c24e16151e8531e0e9ab4200fc643731b
darryl west <dpw@raincitysoftware.com>
2022-09-04 13:03:01 -0500

    added test data

--- pipe_viewer/src/main.rs
+++ pipe_viewer/src/main.rs
@@ -1,9 +1,12 @@
-// use std::env;
+use std::env;
 use std::io::{self, Read, Write};
 
 const CHUNK_SIZE: usize = 16 * 1024;
 
 fn main() {
+    let silent = !env::var("PV_SILENT").unwrap_or_default().is_empty();
+    let verbose = !env::var("PV_VERBOSE").unwrap_or_default().is_empty();
+
     let mut total_bytes = 0;
 
     loop {
@@ -15,8 +18,13 @@ fn main() {
         };
 
         total_bytes += byte_count;
-        io::stdout().write_all(&buffer[..byte_count]).unwrap();
+
+        if verbose {
+            io::stdout().write_all(&buffer[..byte_count]).unwrap();
+        }
     }
 
-    eprintln!("Total Byte Count: {}", total_bytes);
+    if !silent {
+        eprintln!("Total Byte Count: {}", total_bytes);
+    }
 }
