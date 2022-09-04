2f0cfb6994b3bf42909e420f0af7e715da18b8a6
darryl west <dpw@raincitysoftware.com>
2022-09-04 12:35:05 -0500

    1st pipe viewer iteration

--- pipe_viewer/src/main.rs
+++ pipe_viewer/src/main.rs
@@ -1,3 +1,22 @@
+// use std::env;
+use std::io::{self, Read, Write};
+
+const CHUNK_SIZE: usize = 16 * 1024;
+
 fn main() {
-    println!("Hello, world!");
+    let mut total_bytes = 0;
+
+    loop {
+        let mut buffer = [0; CHUNK_SIZE];
+        let byte_count = match io::stdin().read(&mut buffer) {
+            Ok(0) => break,
+            Ok(x) => x,
+            Err(_) => break,
+        };
+
+        total_bytes += byte_count;
+        io::stdout().write_all(&buffer[..byte_count]).unwrap();
+    }
+
+    eprintln!("Total Byte Count: {}", total_bytes);
 }
