94f0845be1a1d402b441dc5f6065a51945f66a0b
darryl west <dpw@raincitysoftware.com>
2022-09-15 12:17:09 -0700

    create a lib to replace bin

--- btree/src/lib.rs
+++ btree/src/lib.rs
@@ -1,12 +1,12 @@
 
 #[derive(Debug)]
-enum BinaryTree<T> {
+pub enum BinaryTree<T> {
     Empty,
     NonEmpty(Box<TreeNode<T>>)
 }
 
 impl<T: Ord> BinaryTree<T> {
-    fn add(&mut self, value: T) {
+    pub fn add(&mut self, value: T) {
         match *self {
             BinaryTree::Empty => {
                 *self = BinaryTree::NonEmpty(Box::new(TreeNode {
@@ -27,7 +27,7 @@ impl<T: Ord> BinaryTree<T> {
 }
 
 impl<T: Clone> BinaryTree<T> {
-    fn walk(&self) -> Vec<T> {
+    pub fn walk(&self) -> Vec<T> {
         match *self {
             BinaryTree::Empty => vec![],
             BinaryTree::NonEmpty(ref boxed) => {
@@ -41,24 +41,28 @@ impl<T: Clone> BinaryTree<T> {
 }
 
 #[derive(Debug)]
-struct TreeNode<T> {
+pub struct TreeNode<T> {
     element: T,
     left: BinaryTree<T>,
     right: BinaryTree<T>,
 }
 
-fn main() {
-    let list = vec![4, 6, 9, 7, 3, 2, 5];
-    println!("original: {:?}", &list);
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    let mut root: BinaryTree<u8> = BinaryTree::Empty;
+    #[test]
+    fn test_build() {
+        let list = vec![4, 6, 9, 7, 3, 2, 5];
+        // println!("original: {:?}", &list);
 
-    for x in list {
-        root.add(x);
+        let mut root: BinaryTree<u8> = BinaryTree::Empty;
+
+        for x in list {
+            root.add(x);
+        }
+
+        let sorted = root.walk();
+        assert_eq!(sorted, vec![2, 3, 4, 5, 6, 7, 9]);
     }
-
-
-    let sorted = root.walk();
-    println!("sorted: {:?}", &sorted);
-
 }
