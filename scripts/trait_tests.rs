1d1966af05697e09216b91d8acc3dd2766f4f0ce
darryl west <dpw@raincitysoftware.com>
2022-09-24 16:09:22 -0700

    traits

--- scripts/trait_tests.rs
+++ scripts/trait_tests.rs
@@ -1,17 +1,39 @@
 #!/usr/bin/env rust-script
 
+#![allow(unused)]
+
 use std::io::Write;
-// use std::fs::File;
+use std::fs::File;
 
-// fn say_hello<W: Write() -> std::io::Result<()> 
+pub fn say_hi<W: Write>(out: &mut W) -> std::io::Result<()>  {
+    let _ = out.write(b"hello with more text\n");
+    out.flush()
+}
 
-fn main() {
+pub fn write_bytes(out: &mut dyn Write) -> std::io::Result<()> {
+    let _ = out.write(b"hello with more text from the byte writer\n");
+    out.flush()
+}
+
+fn show(buf: Vec<u8>) {
+    buf.into_iter().map(|x| x as char).for_each(|ch| print!("{}", ch));
+}
+
+fn test_trait_object() {
     let mut buf: Vec<u8> = vec![];
     let writer: &mut dyn Write = &mut buf;
 
-    writer.write_all(b"this is a block of text\n");
+    let _ = writer.write_all(b"this is a block of text\n");
+    show(buf);
+}
 
-    buf.into_iter().map(|x| x as char).for_each(|ch| print!("{}", ch));
+fn main() {
+    test_trait_object();
 
+    let mut buf: Vec<u8> = vec![];
+    write_bytes(&mut buf);
+    show(buf);
 
+    let mut fout = File::create("/tmp/hello.txt").unwrap();
+    write_bytes(&mut fout);
 }
