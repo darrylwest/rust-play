b4b01d1841f1345af6e5c0b35cfd00992f72b6cc
darryl west <dpw@raincitysoftware.com>
2022-09-27 16:42:41 -0700

    added echo server and copy

---
+++ redis-test/src/bin/server.rs
@@ -0,0 +1,65 @@
+
+use tokio::net::{TcpListener, TcpStream};
+use mini_redis::{Connection, Frame};
+
+use bytes::Bytes;
+use std::collections::HashMap;
+
+// TODO consider using parking_log::Mutex ...
+use std::sync::{Arc, Mutex};
+
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
+
+#[tokio::main]
+async fn main() {
+    let host = "127.0.0.1:6379";
+    let listener = TcpListener::bind(host).await.unwrap();
+
+    println!("listening on {}", host);
+
+    // TODO replace HashMap with dash map
+    let db = Arc::new(Mutex::new(HashMap::new()));
+
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
+        let db = db.clone();
+
+        println!("new connection: {:?}", socket);
+
+        // creates a green thread or a task using 64 bytes of overhead...
+        tokio::spawn(async move {
+            process(socket, db).await;
+        });
+    }
+}
+
+async fn process(socket: TcpStream, db: Db) {
+    use mini_redis::Command::{self, Get, Set};
+
+    let mut connection = Connection::new(socket);
+
+    while let Some(frame) = connection.read_frame().await.unwrap() {
+        let response = match Command::from_frame(frame).unwrap() {
+            Set(cmd) => {
+                println!("cmd: {:?}", cmd);
+                let mut db = db.lock().unwrap();
+                db.insert(cmd.key().to_string(), cmd.value().clone());
+                println!("db: {:?}", db.clone());
+
+                Frame::Simple("OK".to_string())
+            }
+            Get(cmd) => {
+                println!("cmd: {:?}", cmd);
+                let db = db.lock().unwrap();
+                if let Some(value) = db.get(cmd.key()) {
+                    Frame::Bulk(value.clone().into())
+                } else {
+                    Frame::Null
+                }
+            }
+            cmd => panic!("unimplemented command: {:?}", cmd),
+        };
+
+        connection.write_frame(&response).await.unwrap();
+    }
+}
--- redis-test/src/main.rs
+++ redis-test/src/main.rs
@@ -1,65 +1,85 @@
 
-
-use tokio::net::{TcpListener, TcpStream};
-use mini_redis::{Connection, Frame};
-
+use mini_redis::client;
 use bytes::Bytes;
-use std::collections::HashMap;
+use tokio::sync::{mpsc, oneshot};
 
-// TODO consider using parking_log::Mutex ...
-use std::sync::{Arc, Mutex};
+#[derive(Debug)]
+enum Command {
+    Get {
+        key: String,
+        resp: Responder<Option<Bytes>>,
+    },
+    Set {
+        key: String,
+        val: Bytes,
+        resp: Responder<()>,
+    },
+}
 
-type Db = Arc<Mutex<HashMap<String, Bytes>>>;
+type Responder<T> = oneshot::Sender<mini_redis::Result<T>>;
 
 #[tokio::main]
 async fn main() {
+    let (tx, mut rx) = mpsc::channel(32);
+    let tx2 = tx.clone();
+
     let host = "127.0.0.1:6379";
-    let listener = TcpListener::bind(host).await.unwrap();
 
-    println!("listening on {}", host);
+    let manager = tokio::spawn(async move {
+        let mut client = client::connect(host).await.unwrap();
 
-    let db = Arc::new(Mutex::new(HashMap::new()));
+        while let Some(cmd) = rx.recv().await {
+            match cmd {
+                Command::Get { key, resp } => {
+                    let res = client.get(&key).await;
 
-    loop {
-        let (socket, _) = listener.accept().await.unwrap();
-        let db = db.clone();
+                    let _ = resp.send(res);
+                }
+                Command::Set { key, val, resp } => {
+                    let res = client.set(&key, val).await;
 
-        println!("new connection: {:?}", socket);
-
-        // creates a green thread or a task using 64 bytes of overhead...
-        tokio::spawn(async move {
-            process(socket, db).await;
-        });
-    }
-}
-
-async fn process(socket: TcpStream, db: Db) {
-    use mini_redis::Command::{self, Get, Set};
-
-    let mut connection = Connection::new(socket);
-
-    while let Some(frame) = connection.read_frame().await.unwrap() {
-        let response = match Command::from_frame(frame).unwrap() {
-            Set(cmd) => {
-                println!("cmd: {:?}", cmd);
-                let mut db = db.lock().unwrap();
-                db.insert(cmd.key().to_string(), cmd.value().clone());
-                println!("db: {:?}", db.clone());
-
-                Frame::Simple("OK".to_string())
-            }
-            Get(cmd) => {
-                println!("cmd: {:?}", cmd);
-                let db = db.lock().unwrap();
-                if let Some(value) = db.get(cmd.key()) {
-                    Frame::Bulk(value.clone().into())
-                } else {
-                    Frame::Null
+                    let _ = resp.send(res);
                 }
             }
-            cmd => panic!("unimplemented command: {:?}", cmd),
+        }
+    });
+
+    let t1 = tokio::spawn(async move {
+        let (resp_tx, resp_rx) = oneshot::channel();
+        let cmd = Command::Get {
+            key: "foo".to_string(),
+            resp: resp_tx,
         };
 
-        connection.write_frame(&response).await.unwrap();
-    }
+        if tx.send(cmd).await.is_err() {
+            eprintln!("connection task shutdown");
+            return;
+        }
+
+        let res = resp_rx.await;
+        println!("Got get: {:?}", res);
+    });
+
+    let t2 = tokio::spawn(async move {
+        let (resp_tx, resp_rx) = oneshot::channel(); 
+        let cmd = Command::Set {
+            key: "foo".to_string(),
+            val: "bar".into(),
+            resp: resp_tx,
+        };
+
+        if tx2.send(cmd).await.is_err() {
+            eprintln!("connection task shutdown");
+            return;
+        }
+
+        let res = resp_rx.await;
+        println!("Got set = {:?}", res);
+    });
+
+    t1.await.unwrap();
+    t2.await.unwrap();
+
+    manager.await.unwrap();
+
 }
