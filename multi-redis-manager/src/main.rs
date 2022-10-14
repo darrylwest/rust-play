929330e977ab3c59f8af08dc5111037fa5920b2f
darryl west <dpw@raincitysoftware.com>
2022-10-14 14:09:21 -0700

    run redis with anyhow errors

--- multi-redis-manager/src/main.rs
+++ multi-redis-manager/src/main.rs
@@ -1,27 +1,36 @@
+use anyhow::Result;
 use subprocess::{Exec, Redirection};
 
+fn show_utf8(data: Vec<u8>) {
+    match String::from_utf8(data) {
+        Ok(s) => println!("{}", s),
+        Err(e) => println!("utf8 error: {}", e),
+    }
+}
+
 // create a struct for this
-fn run_redis() {
-    let result = Exec::cmd("ls")
+fn run_redis() -> Result<()> {
+    let result = Exec::cmd("redis-server")
         .stdout(Redirection::Pipe)
         .stderr(Redirection::Merge)
         .capture();
 
     match result {
-        Ok(data) => match String::from_utf8(data.stdout) {
-            Ok(s) => println!("{}", s),
-            Err(e) => println!("{}", e),
-        }
+        Ok(data) => show_utf8(data.stdout),
         Err(e) => eprintln!("error: {}", e),
     }
-    
+
+    Ok(())
 }
 
-fn main() {
+fn main() -> Result<()> {
     // read config
+    // let config = std::fs::read_to_string()
     // start the instances with async (begin with simple threads)
 
-    run_redis();
+    run_redis()?;
 
     // add shutdown logic with messaging
+
+    Ok(())
 }
