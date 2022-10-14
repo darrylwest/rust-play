c9854f6fad3b677ca98a82a26f260c8d10579310
darryl west <dpw@raincitysoftware.com>
2022-10-14 13:57:32 -0700

    basic tests

--- multi-redis-manager/src/main.rs
+++ multi-redis-manager/src/main.rs
@@ -1,3 +1,27 @@
+use subprocess::{Exec, Redirection};
+
+// create a struct for this
+fn run_redis() {
+    let result = Exec::cmd("ls")
+        .stdout(Redirection::Pipe)
+        .stderr(Redirection::Merge)
+        .capture();
+
+    match result {
+        Ok(data) => match String::from_utf8(data.stdout) {
+            Ok(s) => println!("{}", s),
+            Err(e) => println!("{}", e),
+        }
+        Err(e) => eprintln!("error: {}", e),
+    }
+    
+}
+
 fn main() {
-    println!("Hello, world!");
+    // read config
+    // start the instances with async (begin with simple threads)
+
+    run_redis();
+
+    // add shutdown logic with messaging
 }
