32a3657e10a2d2d87e44d272198ad96fbfbd2416
darryl west <dpw@raincitysoftware.com>
2022-10-26 16:35:38 -0700

    save key/iv as base64

--- openssl-tests/src/main.rs
+++ openssl-tests/src/main.rs
@@ -1,7 +1,6 @@
-// use std::env;
-// use num_bigint::BigUint;
-// use rand::prelude::*;
+use std::env;
 use openssl::symm::{encrypt, decrypt, Cipher};
+use openssl::base64::encode_block;
 use std::iter::repeat_with;
 
 fn gen_key() -> Vec<u8> {
@@ -17,6 +16,25 @@ fn gen_iv(size: u8) -> Vec<u8> {
     let iv: Vec<u8> = repeat_with(|| rng.u8(..)).take(sz).collect();
 
     iv
+}
+
+fn save_key(key: &Vec<u8>, iv: &Vec<u8>) {
+    let sz = key.len() + iv.len();
+    let mut v = Vec::with_capacity(sz);
+
+    for n in key {
+        v.push(*n);
+    }
+
+    for n in iv {
+        v.push(*n);
+    }
+
+    let b64 = encode_block(v.as_slice());
+    println!("{:?}", &v);
+    println!("{}", b64);
+
+    // v
 }
 
 fn enc_dec() {
@@ -35,6 +53,8 @@ fn enc_dec() {
     // println!("iv size: {}", iv_size);
     let iv = gen_iv(iv_size);
 
+    save_key(&key, &iv);
+
     let ciphertext = encrypt(cipher, &key, Some(&iv), data).unwrap();
     println!("{:?}", ciphertext);
 
@@ -46,8 +66,7 @@ fn enc_dec() {
     assert_eq!(original, decrypt_text);
 }
 
-fn main() {
-    /*
+pub fn check_version() {
     if let Ok(v) = env::var("DEP_OPENSSL_VERSION_NUMBER") {
         let version = u64::from_str_radix(&v, 16).unwrap();
 
@@ -55,7 +74,9 @@ fn main() {
             println!("cargo:rustc-cfg=openssl111");
         }
     }
-    */
+}
+
+fn main() {
 
     enc_dec();
 }
