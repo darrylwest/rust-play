78f76cd36be7b83c87a07e45d9b07b808dd4219c
darryl west <dpw@raincitysoftware.com>
2022-10-26 12:47:49 -0700

    added str String and Box to type names

--- scripts/type_names.rs
+++ scripts/type_names.rs
@@ -12,10 +12,16 @@ fn main() {
   let b = 0.4;
   let c = 44i64;
   let d = 55.0f32;
+  let e = "this is a str";
+  let f = String::from("this is a string");
+  let g: Box<&str> = Box::new("this is a box test");
 
 
   println!("type of a: {} -> {}", type_of(a), a);
   println!("type of b: {} -> {}", type_of(b), b);
   println!("type of c: {} -> {}", type_of(c), c);
   println!("type of d: {} -> {}", type_of(d), d);
+  println!("type of e: {} -> {}", type_of(e), e);
+  println!("type of f: {} -> {}", type_of(f.clone()), f.clone());
+  println!("type of g: {} -> {}", type_of(g.clone()), g.clone());
 }
