de71b964dfda219d28075a4e7d480bfaefe56393
darryl west <dpw@raincitysoftware.com>
2022-10-31 13:03:48 -0700

    validation tests

---
+++ validation-tests/src/main.rs
@@ -0,0 +1,50 @@
+use std::borrow::Cow;
+use serde::Deserialize;
+use validator::{Validate, ValidationError};
+
+#[derive(Debug, Validate, Deserialize)]
+struct SignupData {
+    #[validate(email)]
+    email: Cow<'static, str>,
+    #[validate(length(min = 0), custom = "validate_phone")]
+    phone: Cow<'static, str>,
+    #[validate(url)]
+    site: Cow<'static, str>,
+    #[validate(length(min = 1), custom = "validate_unique_username")]
+    first_name: Cow<'static, str>,
+    #[validate(range(min = 18, max = 30))]
+    age: u32,
+}
+
+fn validate_phone(phone: &str) -> Result<(), ValidationError> {
+    match phone.len() {
+        0 => Ok(()),
+        10 => Ok(()),
+        _ => Err(ValidationError::new("bad phone")),
+    }
+}
+
+fn validate_unique_username(username: &str) -> Result<(), ValidationError> {
+    if username == "xXxBadxXx" {
+        return Err(ValidationError::new("a bad user name"));
+    }
+
+    Ok(())
+}
+
+fn main() {
+    let sd = SignupData{
+        email: Cow::from("dpw@raincitysoftware.com"),
+        phone: Cow::from("5551236666"),
+        site: Cow::from("https://raincitysoftware.com"),
+        first_name: Cow::from("darryl"),
+        age: 29,
+    };
+
+    match sd.validate() {
+        Ok(_) => println!("{:?} is valid", sd),
+        Err(e) => println!("{:?} error: {}", sd, e),
+    }
+
+
+}
