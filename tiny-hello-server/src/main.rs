b8da1012de27f1f05566c3c8b3388bca7f3db967
darryl west <dpw@raincitysoftware.com>
2022-11-14 11:03:34 -0800

    updates to openssl version and tests for tiny http

--- tiny-hello-server/src/main.rs
+++ tiny-hello-server/src/main.rs
@@ -2,6 +2,8 @@ extern crate tiny_http;
 
 use std::sync::Arc;
 use std::thread;
+use tiny_http::Response;
+use std::time::SystemTime;
 
 
 fn main() {
@@ -20,21 +22,33 @@ fn main() {
                 let url = rq.url().to_string();
                 // let path = Path::new(&url);
                 
-                println!("rq {:?} {:?} {:?}", rq.method(), &url, rq.headers());
+                println!("req {:?} {:?} {:?}", rq.method(), &url, rq.headers());
 
                 match url.as_str() {
                     "/hello" => {
-                        let response = tiny_http::Response::from_string("hello world".to_string());
+                        let response = Response::from_string("hello world".to_string());
                         let _ = rq.respond(response);
                     }
 
                     "/goodbye" => {
-                        let response = tiny_http::Response::from_string("good bye".to_string());
+                        let response = Response::from_string("good bye".to_string());
+                        let _ = rq.respond(response);
+                    }
+
+                    "/v1/ping" => {
+                        let p = ping();
+                        let response = Response::from_string(p);
+                        let _ = rq.respond(response);
+                    }
+
+                    "/v1/status" => {
+                        let ss = status();
+                        let response = Response::from_string(ss);
                         let _ = rq.respond(response);
                     }
 
                     _ => {
-                        let response = tiny_http::Response::from_string("what?".to_string());
+                        let response = Response::from_string("what?".to_string());
                         let _ = rq.respond(response);
                     }
                 }
@@ -46,5 +60,15 @@ fn main() {
     for h in handles {
         h.join().unwrap();
     }
+
+}
+
+fn ping() -> String {
+    "PONG".to_string()
+}
+
+fn status() -> String {
+    let now = SystemTime::now();
+    format!("i'm ok, at lease at {:?}", now)
 }
 
