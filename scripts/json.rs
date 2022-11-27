b53a8f408f1a58336f1bd58e1d2be5c5080e0806
darryl west <darryl.west@RainCitySoftware.com>
2022-11-27 11:43:24 -0800

    runner tests

--- scripts/json.rs
+++ scripts/json.rs
@@ -1,10 +1,28 @@
 //: -s
 
 use json;
+// use serde_json::Serialize;
+
+#[derive(Debug, Clone)]
+struct Resp {
+    code: u16,
+    success: bool,
+    payload: String,
+}
 
 let parsed = json::parse(r#"{"code":200,"success":true,"payload":{"features":["awsome","easyAPI","lowcurve"]}}"#)?;
 
 println!("{}", parsed);
+
+
+let obj = Resp {
+    code: 200,
+    success: true,
+    payload: "hello world".to_owned(),
+};
+
+println!("obj: {:?}", obj);
+// println!("json: {}", serde_json::to_string(obj));
 
 // a runner script: https://crates.io/crates/runner
 // do this first: runner --add "time json regex"
