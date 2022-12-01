ca8d6ac0c6230e02c4a3c5951e41697116045cd1
darryl west <dpw@raincitysoftware.com>
2022-12-01 14:32:52 -0800

    tests for ssh2

--- ssh2-tests/src/main.rs
+++ ssh2-tests/src/main.rs
@@ -2,42 +2,36 @@
 use anyhow::Result;
 use std::net::TcpStream;
 use ssh2::Session;
+use std::io::Read;
 // use std::path::Path;
 
 fn main() -> Result<()> {
-    let sess = Session::new().expect("should create a session");
-    let mut agent = sess.agent()?;
-
-    println!("connect to the agent");
-    agent.connect()?;
-    agent.list_identities()?;
-
-    for identity in agent.identities().unwrap() {
-        println!("{}", identity.comment());
-        println!("{:?}", identity.blob());
-
-        // let r = agent.userauth("dpw", &identity);
-        // println!("{:?}", r);
-    }
-
-    Ok(())
-}
-
-pub fn connect() -> Result<()> {
-    let addr = "10.0.1.105:22";
-    // let addr = "tiburon.local:22";
-    let tcp = TcpStream::connect(addr)?;
+    // let addr = "10.0.1.105:22";
+    let addr = "piedmont:22";
+    let tcp = TcpStream::connect(addr).unwrap();
     println!("connected to {}", addr);
 
-    let mut sess = Session::new()?;
+    let mut sess = Session::new().unwrap();
     sess.set_tcp_stream(tcp);
     sess.handshake()?;
-    // println!("session ok");
+    println!("session ok");
 
-    sess.userauth_agent("dpw")?;
-    println!("auth via agent dpw");
+    let pw = "";
+    sess.userauth_password("dpw", pw).unwrap();
+    println!("auth via dpw");
 
     assert!(sess.authenticated());
 
+    let mut channel = sess.channel_session().unwrap();
+    channel.exec("ls -la").unwrap();
+
+    let mut buf = String::new();
+    channel.read_to_string(&mut buf).unwrap();
+    println!("{}", buf);
+
+    channel.wait_close()?;
+    println!("{}", channel.exit_status()?);
+
     Ok(())
 }
+
