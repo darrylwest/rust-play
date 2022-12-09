35e618e1fcfc2d41bcd27a2a4ab985891f75fd2e
darryl west <dpw@raincitysoftware.com>
2022-12-09 10:25:22 -0800

    added basic commands and responses

--- udp-net/src/main.rs
+++ udp-net/src/main.rs
@@ -1,6 +1,8 @@
 
 use tokio::net::UdpSocket;
 use std::io;
+use std::time::{SystemTime, UNIX_EPOCH};
+use std::borrow::Cow;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
@@ -10,7 +12,7 @@ async fn main() -> io::Result<()> {
     let sock = UdpSocket::bind(addr).await?;
     loop {
         // listen for a message
-        let mut buf = [0; 1024];
+        let mut buf = [0; 128];
         println!("wait for a connection...");
 
         let (len, addr) = sock.recv_from(&mut buf).await?;
@@ -18,10 +20,40 @@ async fn main() -> io::Result<()> {
         println!("recv: {} bytes from {:?}, msg: {}", len, addr, msg);
 
         // parse the message to create a response
-        let response = "ok\r\n";
+        let response = match msg {
+            Cow::Borrowed("/now\n") => {
+                let tm = get_now();
+                tm.to_string()
+            },
+            Cow::Borrowed("/ms\n") => {
+                let tm = get_now_ms();
+                tm.to_string()
+            },
+            Cow::Borrowed("/ping\n") => String::from("pong\r\n"),
+            _ => String::from("error\r\n"),
+        };
+
 
         // return the response
         let len = sock.send_to(response.as_bytes(), addr).await?;
         println!("returned: {:?}, size {}.", response, len);
     }
+}
+
+fn get_now() -> String {
+    let now = match SystemTime::now().duration_since(UNIX_EPOCH) {
+        Ok(n) => n.as_secs(),
+        _ => 0_u64,
+    };
+
+    format!("{}\r\n", now)
+}
+
+fn get_now_ms() -> String {
+    let now = match SystemTime::now().duration_since(UNIX_EPOCH) {
+        Ok(n) => n.as_millis(),
+        _ => 0_u128,
+    };
+
+    format!("{}\r\n", now)
 }
