105830025c661ebec19acfe92161db86b091fad1
darryl west <dpw@raincitysoftware.com>
2022-12-10 19:14:28 -0800

    top ten version

--- scripts/mesh-status.rs
+++ scripts/mesh-status.rs
@@ -7,6 +7,8 @@ fn main() {
     send("get/now");
     send("get/millis");
     send("get/status");
+    // get the key using the top-ten version
+    send("get/key/7eQ5yEDKv7kR");
 }
 
 fn send(req: &str) {
@@ -16,7 +18,8 @@ fn send(req: &str) {
     socket.connect("10.0.1.142:22200").expect("connect to remote");
     socket.send(req.as_bytes()).expect("send to error");
     
-    let mut buf = [0; 1024];
+    const MAX_DATAGRAM_SIZE: usize = 4096;
+    let mut buf = [0; MAX_DATAGRAM_SIZE];
     match socket.recv(&mut buf) {
       Ok(r) => {
         let s = String::from_utf8_lossy(&buf[..r]);
