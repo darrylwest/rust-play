b707040fe9f2d41e3c5e2677671faf29cb362081
darryl west <dpw@raincitysoftware.com>
2022-12-11 11:45:44 -0800

    added random word generator

---
+++ random-words/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    println!("Hello, world!");
+    println!("retires stay heine stookers maze");
+    println!("abates moonish pessimism solemnize morbus");
+    println!("corporator mosel chessel mead coalitions");
+}
