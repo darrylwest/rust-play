f88d7f61548c349ff33d7702e422fed880acafce
darryl west <dpw@raincitysoftware.com>
2023-01-06 10:24:58 -0800

    more cow tests

--- scripts/env_var.rs
+++ scripts/env_var.rs
@@ -2,18 +2,27 @@
 
 use std::env;
 use std::borrow::Cow;
+use std::fmt::Write;
 
-
-fn get_var(key: &str) -> Cow<'static, str> {
-    env::var(key)
+fn get_var(key: &str, title: Option<String>) -> Cow<'static, str> {
+    let mut name: Cow<'static, str> = env::var(key)
         .map(|v| Cow::Owned(v))
-        .unwrap_or(Cow::Borrowed("no env var with that name"))
+        .unwrap_or(key.to_string().into());
+
+    if let Some(ttl) = title {
+        write!(name.to_mut(), ", {}", ttl).unwrap();
+    }
+
+    name
 }
 
 fn main() {
-    let name = get_var("HOME");
+    let name = get_var("HOME", Some("Phd".to_string()));
     println!("HOME={:?}", name);
 
-    let name = get_var("xHOME");
+    let name = get_var("my-empty-var", None);
+    println!("xHOME={:?}", name);
+
+    let name = get_var("rrr", Some("III".to_string()));
     println!("xHOME={:?}", name);
 }
