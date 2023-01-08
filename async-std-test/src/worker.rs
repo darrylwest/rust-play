fdc5ef34ed456d29d9c6d4ace628e9d9a6f68a82
darryl west <dpw@raincitysoftware.com>
2023-01-08 12:04:42 -0800

    added worker

--- async-std-test/src/worker.rs
+++ async-std-test/src/worker.rs
@@ -1,7 +1,9 @@
-/// general purpose worker
-// use anyhow::Result;
 use async_channel::bounded;
 use async_channel::Sender;
+/// general purpose worker
+// use anyhow::Result;
+use log::info;
+use std::time::Instant;
 
 #[derive(Debug, Default, Clone)]
 pub enum Command {
@@ -18,28 +20,72 @@ pub enum WorkerState {
 
 #[derive(Debug, Clone)]
 pub struct Worker {
-    pub state: WorkerState,
+    id: String,
+    started_at: Instant,
     request_tx: Sender<Command>,
 }
 
 impl Worker {
+    /// create and start a new worker.
     pub async fn new() -> Worker {
+        let started_at = Instant::now();
+        let id = "worker-1";
+        let state = WorkerState::Idle;
+
+        info!("starting up worker, id: {}", id);
+
         let (tx, request_receiver) = bounded(250);
 
-        // now
+        let worker = Worker {
+            id: id.to_string(),
+            started_at,
+            request_tx: tx,
+        };
+
         async_std::task::spawn(async move {
+            // now read and respond to requests
+
             request_receiver.close();
         });
 
-        Worker {
-            state: WorkerState::Idle,
-            request_tx: tx,
-        }
+        info!("worker created: {:?}, state: {:?}", &worker, state);
+
+        // return a reference to this worker
+        worker
+    }
+
+    /// return the worker's id
+    pub fn worker_id(&self) -> String {
+        self.id.to_string()
+    }
+
+    /// return the number of seconds this worker has been alive
+    pub fn get_uptime(&self) -> u64 {
+        let secs = self.started_at.elapsed().as_secs();
+
+        info!("uptime {} seconds", secs);
+
+        secs
     }
 
     /// This is invoked by the client to enable sending command request to
     /// the worker
     pub fn request_channel(&self) -> Sender<Command> {
         self.request_tx.clone()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new() {
+        async_std::task::block_on(async move {
+            let worker = Worker::new().await;
+            println!("worker: {:?}", worker);
+
+            assert_eq!(worker.get_uptime(), 0);
+        });
     }
 }
