f7080b160f969dcce0321ebbc460085c82afd02b
darryl west <dpw@raincitysoftware.com>
2023-01-09 07:35:11 -0800

    channel tests

--- async-std-test/src/worker.rs
+++ async-std-test/src/worker.rs
@@ -1,3 +1,4 @@
+use crate::JsonString;
 /// general purpose worker
 // use anyhow::Result;
 use anyhow::Result;
@@ -7,7 +8,6 @@ use log::*;
 use serde::{Deserialize, Serialize};
 use std::iter::repeat_with;
 use std::time::Instant;
-use crate::JsonString;
 
 #[derive(Debug, Clone)]
 pub enum Command {
@@ -168,6 +168,47 @@ mod tests {
             let cmd = Command::Shutdown;
             let ok = request_channel.send(cmd).await.is_ok();
             assert_eq!(ok, true);
+        });
+    }
+
+    #[test]
+    fn bounded_tests() {
+        async_std::task::block_on(async move {
+            let (s, r) = async_channel::bounded(2);
+            assert_eq!(r.is_empty(), true);
+            assert_eq!(s.send(10).await, Ok(()));
+            assert_eq!(s.send(12).await, Ok(()));
+
+            assert_eq!(r.is_full(), true);
+            assert_eq!(r.recv().await, Ok(10));
+            assert_eq!(r.recv().await, Ok(12));
+            assert_eq!(r.is_empty(), true);
+
+            // second test
+            println!("r empty? {}", r.is_empty());
+            assert_eq!(s.send(14).await, Ok(()));
+            assert_eq!(s.send(16).await, Ok(()));
+
+            // if you try sending more than the buffer allows here, the
+            // process will just wait until there is room in the queue
+
+            println!("r empty? {}", r.is_empty());
+            assert_eq!(r.recv().await, Ok(14));
+            assert_eq!(r.recv().await, Ok(16));
+
+            assert_eq!(s.close(), true);
+            assert_eq!(s.is_closed(), true);
+
+            // closing the sender shuts down the receiver as well
+            assert_eq!(r.is_closed(), true);
+
+            match r.recv().await {
+                Ok(_) => assert!(false, "should not work here"),
+                Err(e) => {
+                    println!("error: {:?}", e);
+                    assert!(true);
+                }
+            }
         });
     }
 }
