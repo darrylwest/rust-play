1991ff592de260724f825bac34d9038081b88081
darryl west <dpw@raincitysoftware.com>
2023-01-11 07:36:08 -0800

    multi-type hash map tests

--- scripts/hash-non-type.rs
+++ scripts/hash-non-type.rs
@@ -1,10 +1,33 @@
 #!/usr/bin/env rust-script
 // cargo-deps: rand
 
-// use std::any::{Any, TypeId};
+use std::any::{Any, TypeId};
 use std::collections::HashMap;
 use rand::prelude::*;
 use std::char::from_digit;
+
+fn main() {
+    let mut hmap: HashMap<String, Box<dyn Any>> = HashMap::new();
+
+    let v1: Box<dyn Any> = Box::new(3_i32);
+    let v2: Box<dyn Any> = Box::new("this is a test");
+    let v3: Box<dyn Any> = Box::new("this is a test".to_string());
+    let v4: Box<dyn Any> = Box::new(vec!["a", "b", "c"]);
+
+    println!("v1 is i32: {}", (&*v1).type_id() == TypeId::of::<i32>());
+    println!("v2 is &str: {}", (&*v2).type_id() == TypeId::of::<&str>());
+    println!("v3 is String: {}", (&*v3).type_id() == TypeId::of::<String>());
+    println!("v2 is vec: {}", (&*v4).type_id() == TypeId::of::<&str>());
+
+
+    hmap.insert(create_key(), v1);
+    hmap.insert(create_key(), v2);
+    hmap.insert(create_key(), v3);
+    hmap.insert(create_key(), v4);
+
+    println!("hmap: {:?}", hmap);
+
+}
 
 fn create_key() -> String {
     let mut rng = rand::thread_rng();
@@ -19,15 +42,3 @@ fn create_key() -> String {
 }
 
 
-fn main() {
-    let mut hmap: HashMap<String, String> = HashMap::new();
-
-    let key = create_key();
-
-    println!("key: {}", key);
-
-    hmap.insert(key, "my value".to_string());
-
-    println!("hmap: {:?}", hmap);
-
-}
