76346e203d098c7a6cdc9b4a90b3fef35c2473f0
darryl west <dpw@raincitysoftware.com>
2023-01-11 07:49:07 -0800

    more Any tests

--- scripts/hash-non-type.rs
+++ scripts/hash-non-type.rs
@@ -6,18 +6,26 @@ use std::collections::HashMap;
 use rand::prelude::*;
 use std::char::from_digit;
 
+pub struct Person {
+    pub name: String,
+    pub age: i32,
+}
+
 fn main() {
     let mut hmap: HashMap<String, Box<dyn Any>> = HashMap::new();
+    let p = Person { name: "jonny".to_string(), age: 35 };
 
     let v1: Box<dyn Any> = Box::new(3_i32);
     let v2: Box<dyn Any> = Box::new("this is a test");
     let v3: Box<dyn Any> = Box::new("this is a test".to_string());
     let v4: Box<dyn Any> = Box::new(vec!["a", "b", "c"]);
+    let v5: Box<dyn Any> = Box::new(p); 
 
     println!("v1 is i32: {}", (&*v1).type_id() == TypeId::of::<i32>());
     println!("v2 is &str: {}", (&*v2).type_id() == TypeId::of::<&str>());
     println!("v3 is String: {}", (&*v3).type_id() == TypeId::of::<String>());
-    println!("v2 is vec: {}", (&*v4).type_id() == TypeId::of::<&str>());
+    println!("v4 is vec: {}", (&*v4).type_id() == TypeId::of::<&str>());
+    println!("v5 is vec: {}", (&*v5).type_id() == TypeId::of::<Person>());
 
 
     hmap.insert(create_key(), v1);
@@ -26,6 +34,10 @@ fn main() {
     hmap.insert(create_key(), v4);
 
     println!("hmap: {:?}", hmap);
+
+    for (k, v) in hmap.iter() {
+        println!("{} {:?}", k, (&v));
+    }
 
 }
 
