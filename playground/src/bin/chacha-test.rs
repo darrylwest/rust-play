9df1741b7d49643904f6663d1ae44bf83919c096
darryl west <dpw@raincitysoftware.com>
2023-01-16 07:19:18 -0800

    chacha encrypt/decrypt error clean ups

--- playground/src/bin/chacha-test.rs
+++ playground/src/bin/chacha-test.rs
@@ -1,5 +1,5 @@
 
-use anyhow::Result;
+use anyhow::{Result, anyhow};
 use chacha20poly1305::{
     aead::{Aead, AeadCore, KeyInit, OsRng},
     ChaCha20Poly1305,
@@ -15,20 +15,27 @@ pub struct ChaChaKeys {
     pub nonce: GenericArray<u8, U12>,
 }
 
+/// encrypt the plain text using ChaCha20Poly1305
 pub fn encrypt(cck: &ChaChaKeys, text: &str) -> Result<Vec<u8>> {
 
     let ptext = text.as_bytes();
     let cipher = ChaCha20Poly1305::new(&cck.key);
-    let ciphertext = cipher.encrypt(&cck.nonce, ptext.as_ref()).unwrap();
-
-    Ok(ciphertext)
+    if let Ok(ciphertext) = cipher.encrypt(&cck.nonce, ptext.as_ref()) {
+        Ok(ciphertext)
+    } else {
+        Err(anyhow!("broken cha cha"))
+    }
 }
 
+/// decrypt the cipher text using ChaCha20Poly1305
 pub fn decrypt(cck: &ChaChaKeys, ciphertext: Vec<u8>) -> Result<Vec<u8>> {
     let cipher = ChaCha20Poly1305::new(&cck.key);
-    let vtext = cipher.decrypt(&cck.nonce, ciphertext.as_ref()).unwrap();
 
-    Ok(vtext)
+    if let Ok(vtext) = cipher.decrypt(&cck.nonce, ciphertext.as_ref()) {
+        Ok(vtext)
+    } else {
+        Err(anyhow!("broken cha cha"))
+    }
 }
 
 fn main() -> Result<()> {
@@ -55,19 +62,6 @@ fn main() -> Result<()> {
     println!("decrypted: {}", stext);
 
     assert_eq!(text, stext);
-
-    /*
-
-    let cipher = ChaCha20Poly1305::new(&cck.key);
-    let ciphertext: Vec<u8> = cipher.encrypt(&cck.nonce, ptext.as_ref()).expect("should encrypt");
-    println!("{:?}", ciphertext);
-
-
-    println!("original.: {}", text);
-
-    let plaintext = cipher.decrypt(&nonce, ciphertext.as_ref()).unwrap();
-    println!("decrypted: {}", String::from_utf8(plaintext)?);
-     */
 
     Ok(())
 
