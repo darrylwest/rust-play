fb7b93a9109a07ee02c5a7f462612b5f43758824
darryl west <dpw@raincitysoftware.com>
2023-01-15 17:02:18 -0800

    cha cha updates

--- playground/src/bin/chacha-test.rs
+++ playground/src/bin/chacha-test.rs
@@ -1,18 +1,25 @@
 
+use anyhow::Result;
 use chacha20poly1305::{
     aead::{Aead, AeadCore, KeyInit, OsRng},
     ChaCha20Poly1305
 };
 
-fn main() {
+fn main() -> Result<()> {
     let key = ChaCha20Poly1305::generate_key(&mut OsRng);
     let cipher = ChaCha20Poly1305::new(&key);
     let nonce = ChaCha20Poly1305::generate_nonce(&mut OsRng); // 96-bits; unique per message
 
-    let ciphertext = cipher.encrypt(&nonce, b"plaintext message".as_ref()).unwrap();
+    let text = "my plain text message for the chacha group";
+    let ptext = text.as_bytes();
+    let ciphertext = cipher.encrypt(&nonce, ptext.as_ref()).unwrap();
+
+    println!("original.: {}", text);
 
     let plaintext = cipher.decrypt(&nonce, ciphertext.as_ref()).unwrap();
 
-    assert_eq!(&plaintext, b"plaintext message");
+    println!("decrypted: {}", String::from_utf8(plaintext)?);
+
+    Ok(())
 
 }
