8f110400f6c99cadc25159a45224972ba18c4098
darryl west <dpw@raincitysoftware.com>
2023-01-16 09:53:45 -0800

    url safe base64

--- playground/src/bin/base64-safe.rs
+++ playground/src/bin/base64-safe.rs
@@ -1,18 +1,37 @@
 
 
 use anyhow::Result;
-use base64::{Engine as _, engine::general_purpose};
+use std::env;
 
+/// a url safe base64 encode/decode with no padding
+pub mod url_b64 {
+    use anyhow::Result;
+    use base64::{Engine as _, engine::general_purpose};
+
+    pub fn encode(data: &[u8]) -> String {
+        general_purpose::URL_SAFE_NO_PAD.encode(data)
+    }
+
+    pub fn decode(data: &str) -> Result<Vec<u8>> {
+        let v = general_purpose::URL_SAFE_NO_PAD.decode(data)?;
+
+        Ok(v)
+    }
+}
 
 fn main() -> Result<()> {
-    let data = b"my data stream is every WHERE";
-    let encoded= general_purpose::URL_SAFE_NO_PAD.encode(data);
+    let args: Vec<String> = env::args().skip(1).collect();
+    let data: &[u8] = if args.is_empty() {
+        b"my data stream is every WHERE"
+    } else {
+        args[0].as_bytes()
+    };
+    let encoded = url_b64::encode(data);
 
     println!("{}", encoded);
 
-    let decoded = general_purpose::URL_SAFE_NO_PAD.decode(encoded)?;
+    let decoded = url_b64::decode(&encoded)?;
     println!("{}", String::from_utf8(decoded)?);
-
 
     Ok(())
 }
