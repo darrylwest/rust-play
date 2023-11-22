809ffebe91024a790422a906d28c8e0424697330
darryl west <darryl.west@raincitysoftware.com>
2023-11-22 13:48:19 -0800

    updated daemon project

--- background-runner/src/main.rs
+++ background-runner/src/main.rs
@@ -14,15 +14,15 @@ fn main() {
     let stderr = File::create("./tmp/daemon.err").unwrap();
 
     let daemonize = Daemonize::new()
-        .pid_file("./tmp/daemon.pid") // Every method except `new` and `start`
-        // .chown_pid_file(true)      // is optional, see `Daemonize` documentation
+        .pid_file("daemon.pid") // Every method except `new` and `start`
+        .chown_pid_file(false)      // is optional, see `Daemonize` documentation
         .working_directory("./tmp") // for default behaviour.
-        // .user("nobody")
+        // .user("dpw")
         // .group("daemon") // Group name
         // .group(2)        // or group id.
         .umask(0o777)    // Set umask, `0o027` by default.
-        .stdout(stdout)  // Redirect stdout to `/tmp/daemon.out`.
-        .stderr(stderr)  // Redirect stderr to `/tmp/daemon.err`.
+        .stdout(stdout)  // Redirect stdout to `./tmp/daemon.out`.
+        .stderr(stderr)  // Redirect stderr to `./tmp/daemon.err`.
         .privileged_action(|| "Executed before drop privileges");
 
     match daemonize.start() {
