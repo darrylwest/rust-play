c8a687af597e2d3ad97f88cfcbd3d2e0d2bda6ea
darryl west <darryl.west@RainCitySoftware.com>
2023-12-18 09:55:45 -0800

    merge script

---
+++ merge.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# dpw@alamo
+# 2023-12-18 17:51:09
+#
+
+set -eu
+
+SS=`git status -s`
+
+if [ $SS == "" ]
+then
+    echo "clean"
+else
+    echo "not clean..."
+    exit 0
+fi
+
+
