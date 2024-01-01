b4d85543dcb44524df778a7da9b29a8e39f55ebd
darryl west <darryl.west@raincitysoftware.com>
2024-01-01 08:33:55 -0800

    added tokio to job queue

--- playground/src/bin/queue.rs
+++ playground/src/bin/queue.rs
@@ -1,44 +1,91 @@
 ///
 /// a thread safe queue
 /// 
+use anyhow::Result;
 use std::collections::VecDeque;
+use std::sync::{Arc, RwLock};
+use tokio::time::{sleep, Duration, Instant};
 
 #[derive(Debug, Default, Clone)]
 pub struct Queue<T> {
-    pub queue: VecDeque<T>,
+    pub queue: Arc<RwLock<VecDeque<T>>>,
 }
 
 impl<T> Queue<T> {
     pub fn new() -> Queue<T> {
         Queue {
-            queue: VecDeque::new(),
+            queue: Arc::new(RwLock::new(VecDeque::new())),
         }
     }
 
     /// push an element to the back of the queue
     pub fn push(&mut self, value: T) {
-        self.queue.push_back(value);
+        let mut q = self.queue.write().unwrap();
+        q.push_back(value);
     }
 
     /// pop an element from the front of the queue
     pub fn pop(&mut self) -> Option<T> {
-        self.queue.pop_front()
+        let mut q = self.queue.write().unwrap();
+        q.pop_front()
     }
 
     /// return the number of elements in the queue
     pub fn len(&self) -> usize {
-        self.queue.len()
+        let q = self.queue.read().unwrap();
+        q.len()
     }
 
     /// return true if the queue is empty, else false
     pub fn is_empty(&self) -> bool {
-        self.queue.is_empty()
+        let q = self.queue.read().unwrap();
+        q.is_empty()
     }
 
 }
 
-fn main() {
-    println!("TODO: use Arc and RwLock to create thread safety...");
+#[derive(Debug, Clone)]
+struct Job {
+    name: String,
+    delay: u64,
+}
+
+impl Job {
+    pub fn create(name: &str, delay: u64) -> Job {
+        Job {
+            name: name.to_string(),
+            delay,
+        }
+    }
+
+    /// run the job...
+    pub async fn run(&self) -> Result<()> {
+        sleep(Duration::from_millis(self.delay)).await;
+        println!("Job '{}' completed after {} milliseconds.", self.name, self.delay);
+
+        Ok(())
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let jobs = vec![
+        Job::create("first job", 1_500),
+        Job::create("second job", 350),
+        Job::create("job 3", 750),
+        Job::create("job 4", 650),
+        Job::create("job 5", 850),
+    ];
+
+    let count = jobs.len();
+    let now = Instant::now();
+    for job in jobs {
+        job.run().await?;
+    }
+
+    println!("It took {:?} to complete {} jobs.", now.elapsed(), count);
+
+    Ok(())
 }
 
 #[cfg(test)]
@@ -53,5 +100,8 @@ mod tests {
         let mut queue = queue.clone();
         queue.push(13);
         assert_eq!(queue.len(), 1);
+
+        let value = queue.pop().unwrap();
+        assert_eq!(value, 13);
     }
 }
