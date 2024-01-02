2d279f623da64f51b833e25205d6eccf1f654445
darryl west <darryl.west@RainCitySoftware.com>
2024-01-02 13:22:37 -0800

    shortened timeout

--- udp-client-server/src/bin/client.rs
+++ udp-client-server/src/bin/client.rs
@@ -4,12 +4,14 @@ use std::time::Duration;
 fn main() {
 
     let port = 9001;
-    println!("starting udp client on port {}", port);
-    let server_addr = SocketAddr::from(([10, 0, 1, 237], port));
+    let server_host = [10, 0, 1, 237];
+    println!("starting udp client on host: {:?}, port {}", server_host, port);
+
+    let server_addr = SocketAddr::from((server_host, port));
     let socket = UdpSocket::bind(SocketAddr::from(([0,0,0,0], 0))).expect("connect failed");
 
-    socket.set_read_timeout(Some(Duration::from_secs(5))).expect("set timeout failed");
-    socket.set_write_timeout(Some(Duration::from_secs(5))).expect("set timeout failed");
+    socket.set_read_timeout(Some(Duration::from_secs(2))).expect("set timeout failed");
+    socket.set_write_timeout(Some(Duration::from_secs(2))).expect("set timeout failed");
 
     let msg = "hello from client";
     socket.send_to(msg.as_bytes(), server_addr).expect("failed to send");
