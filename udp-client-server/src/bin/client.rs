48dd8146e8b4890ae05d63de389e56aa9cf30bde
darryl west <darryl.west@RainCitySoftware.com>
2024-01-02 12:05:32 -0800

    read and write timeouts set for client socket

--- udp-client-server/src/bin/client.rs
+++ udp-client-server/src/bin/client.rs
@@ -1,4 +1,5 @@
 use std::net::{UdpSocket, SocketAddr};
+use std::time::Duration;
 
 fn main() {
 
@@ -6,6 +7,9 @@ fn main() {
     println!("starting udp client on port {}", port);
     let server_addr = SocketAddr::from(([10, 0, 1, 237], port));
     let socket = UdpSocket::bind(SocketAddr::from(([0,0,0,0], 0))).expect("connect failed");
+
+    socket.set_read_timeout(Some(Duration::from_secs(5))).expect("set timeout failed");
+    socket.set_write_timeout(Some(Duration::from_secs(5))).expect("set timeout failed");
 
     let msg = "hello from client";
     socket.send_to(msg.as_bytes(), server_addr).expect("failed to send");
