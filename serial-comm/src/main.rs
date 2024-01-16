4c614a5075dbc8938955a4c795b0de745e25644e
darryl west <darryl.west@raincitysoftware.com>
2024-01-16 12:11:54 -0800

    interact with arduino

--- serial-comm/src/main.rs
+++ serial-comm/src/main.rs
@@ -2,20 +2,19 @@
 
 use std::io;
 use std::time::Duration;
-// use std::str::String;
+use std::env;
 
 use serial::prelude::*;
 
 fn main() {
-    let serial_port = "/dev/tty.usbmodemF412FA649B242";
+    // let serial_port = "/dev/tty.usbmodemF412FA649B242";
+    // let mut port = serial::open(serial_port).unwrap();
+    // interact(&mut port).unwrap();
 
-    let mut port = serial::open(serial_port).unwrap();
-    interact(&mut port).unwrap();
-
-    //for arg in env::args_os().skip(1) {
-        //let mut port = serial::open(&arg).unwrap();
-        //interact(&mut port).unwrap();
-    //}
+    for arg in env::args_os().skip(1) {
+        let mut port = serial::open(&arg).unwrap();
+        interact(&mut port).unwrap();
+    }
 }
 
 fn interact<T: SerialPort>(port: &mut T) -> io::Result<()> {
@@ -28,20 +27,20 @@ fn interact<T: SerialPort>(port: &mut T) -> io::Result<()> {
         Ok(())
     })?;
 
-    port.set_timeout(Duration::from_millis(3000))?;
+    port.set_timeout(Duration::from_millis(1000))?;
 
     // prompt for an input
     let buf: Vec<u8> = b"I command you!\n".to_vec();
     port.write(&buf[..])?;
 
 
-    let bufsize = 64;
+    let bufsize = 512;
     loop {
         let mut sbuf: Vec<u8> = vec![0; bufsize];
         let n = port.read(&mut sbuf)?;
 
         let s = String::from_utf8(sbuf).unwrap();
-        print!("n: {} -> {}", n, s);
+        print!("{}", s);
 
         if n < bufsize {
             break;
