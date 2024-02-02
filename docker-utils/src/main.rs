448b8900f5cb91b371761762937841ee61715fee
darryl west <darryl.west@raincitysoftware.com>
2024-02-02 09:19:27 -0800

    docker tests

---
+++ docker-utils/src/main.rs
@@ -0,0 +1,22 @@
+
+extern crate docker;
+
+use docker::Docker;
+
+fn main() {
+    let docker = match Docker::connect("unix:///var/run/docker.sock") {
+        Ok(docker) => docker,
+        Err(e) => {
+            panic!("{}", e);
+        }
+    };
+
+    let containers = match docker.get_containers(false) {
+        Ok(containers) => containers,
+        Err(e) => {
+            panic!("{}", e);
+        }
+    };
+
+    println!("{:?}", containers);
+}
